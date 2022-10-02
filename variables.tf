aeb7114c1064fb9563ad642a79dfa1c20170b68c
Neel Thomas <neeltom92@gmail.com>
2022-10-02 18:53:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 locals {
   cluster_name    = "main"
   cluster_version = "1.22"
-  vpc_id          = "vpc-08ea373114b72653a"
-  aws_account_id  = "596734924894"
+  vpc_id          = "vpc-0123xxx456"
+  aws_account_id  = "123456789"
   aws_region      = "ap-south-1"
-  private_subnets = ["subnet-0643093d9edecd561", "subnet-0dd09aca700cc53d3", "subnet-0ed553255e2d5b6bb"]
-  sshkeys        = "learnarcab"
+  private_subnets = ["subnet-abcd1234567", "subnet-0198765zxywtsu", "subnet-0sqwsdsdsde2db"]
+  sshkeys        = "learna"
 }
